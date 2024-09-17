7934dc24ec79f682cf70f7b4a36ef5b516b5e0e4
thomas.eikhaugen@gmail.com <thomas.eikhaugen@gmail.com>
2024-09-17 20:57:55 +0200

    feat: added registration fetch request

--- src/js/api/auth/register.js
+++ src/js/api/auth/register.js
@@ -1,3 +1,5 @@
+import { API_AUTH_REGISTER } from "../constants";
+
 export async function register({
   name,
   email,
@@ -5,4 +7,36 @@ export async function register({
   bio,
   banner,
   avatar,
-}) {}
+}) {
+  const myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+const raw = JSON.stringify(
+  {
+    "name": name,
+    "email": email,
+    "password": password,
+    "bio": bio, 
+    "avatar": {
+      "url": avatar,
+      "alt": "My avatar alt text"
+    },
+    "banner": {
+      "url": banner, 
+      "alt": "My banner alt text"
+    },
+  }
+);
+
+const requestOptions = {
+  method: "POST",
+  headers: myHeaders,
+  body: raw,
+  redirect: "follow"
+};
+
+fetch(API_AUTH_REGISTER, requestOptions)
+  .then((response) => response.text())
+  .then((result) => console.log(result))
+  .catch((error) => console.error(error));
+}
