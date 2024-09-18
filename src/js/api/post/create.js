d2877e995fd4b26a54369ff0060205a16aca601f
Thomas Eikhaugen <thomas.eikhaugen@gmail.com>
2024-09-18 11:31:57 +0200

    feat: added fetch recuest for post create

--- src/js/api/post/create.js
+++ src/js/api/post/create.js
@@ -1,1 +1,36 @@
-export async function createPost({ title, body, tags, media }) {}
+import { API_SOCIAL_POSTS } from "../constants";
+import { headers as customHeaders } from "../headers"; 
+import { getKey } from "../auth/key";
+
+export async function createPost({ title, body, tags, media }) {
+
+  const myHeaders = new Headers(); 
+  myHeaders.append("Content-Type", "application/json");
+  myHeaders.append("Authorization", getKey()); 
+
+  const apiKeyHeader = customHeaders();
+  const [key, value] = apiKeyHeader.entries().next().value;
+  myHeaders.append(key, value); 
+
+  const raw = JSON.stringify({
+    "title": title,
+    "body": body,
+    "tags": [tags],
+    "media": {
+      "url": media,
+      "alt": "string"
+    }
+  });
+
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow"
+  };
+
+  fetch(API_SOCIAL_POSTS, requestOptions)
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.error(error));
+}
