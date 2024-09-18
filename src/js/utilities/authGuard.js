5d6a2ca128956395fbcfe5b2fdca3d21f51c93f8
Kjetil Harneshaug Hauger <kjehar01278@stud.noroff.no>
2024-09-18 13:23:27 +0200

    CHORE: Added JSDocs
    Added JSDocs

--- src/js/utilities/authGuard.js
+++ src/js/utilities/authGuard.js
@@ -1,3 +1,9 @@
+/**
+ * Checks if the user is authenticated by verifying if the access token exists in localStorage.
+ * If the user is not logged in, it displays an alert and redirects them to the login page.
+ *
+ * @returns {void} Redirects unauthenticated users to the login page.
+ */
 export function authGuard() {
   if (!localStorage.accessToken) {
     alert("You must be logged in to view this page");
