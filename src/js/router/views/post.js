ded73b06f74ae9e7b48c47b89f1b16ddc1f07bf0
Thomas Eikhaugen <thomas.eikhaugen@gmail.com>
2024-09-20 12:45:35 +0200

    feat: added html, added readpostbyid function

--- src/js/router/views/post.js
+++ src/js/router/views/post.js
@@ -10,5 +10,40 @@ function getPostIDFromURL() {
 }
 const postID = getPostIDFromURL()
 
-readPost(postID)
+const postData = readPost(postID)
+console.log(postData)
+
+function displayPostByID(post) {
+    const postData = post.data; // Extract the 'data' property
+
+    const postContainer = document.getElementById('post-container');
+    postContainer.innerHTML = '';
+
+    const postMedia = postData.media && postData.media.url
+        ? `<img class="post-media" src="${postData.media.url}" alt="${postData.media.alt || 'Post media'}">`
+        : '';
+
+    const authorAvatar = postData.author.avatar && postData.author.avatar.url
+        ? `<img class="author-img" src="${postData.author.avatar.url}" alt="${postData.author.name}'s avatar">`
+        : '<img class="author-img" src="/images/default-avatar.png" alt="Default avatar">';
+
+    const postHTML = `
+    <div class="post">
+        ${authorAvatar}
+        <div class="author" data-authorID="${postData.author.name}">
+            <span class="author-name">${postData.author.name}</span>
+        </div>
+        <a href="/post/?postID=${postData.id}" data-postID="${postData.id}">
+            ${postMedia}
+            <h2 class="post-title">${postData.title}</h2>
+            <p class="post-body">${postData.body}</p>
+        </a>
+    </div>
+    `;
+
+    postContainer.innerHTML += postHTML;
+}
+
+
+displayPostByID(postData);
 
