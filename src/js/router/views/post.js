b6161e7a46d03859cc33dc0e49fc43e03bfc76c0
thomas.eikhaugen@gmail.com <thomas.eikhaugen@gmail.com>
2024-09-27 11:06:48 +0200

    chore: refactored getPostIDFromURL function, moved to utilities

--- src/js/router/views/post.js
+++ src/js/router/views/post.js
@@ -1,19 +1,13 @@
 import { readPost } from '../../api/post/read';
 import { setLogoutListener } from '../../ui/global/logout';
+import { getPostIDFromURL } from '../../utilities/getPostIDFromURL';
+import { getLoggedInUserName } from '../../utilities/getLoggedInUserName';
 
 document.addEventListener('DOMContentLoaded', () => {
     setLogoutListener();
     renderPost();
 });
 
-function getPostIDFromURL() {
-    const params = new URLSearchParams(window.location.search);
-    return params.get('postID');
-}
-
-function getLoggedInUserName() {
-    return localStorage.getItem('userID');
-}
 
 async function renderPost() {
     const postID = getPostIDFromURL();
